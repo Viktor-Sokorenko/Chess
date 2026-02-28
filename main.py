e12b1c0e0a80a6515761139450bcef92064676c0
Viktor-Sokorenko <viktor.sokorenko@student.howest.be>
2026-02-28 11:03:32 +0100

    feat: add print board decorator on move

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from board import Board
 
 board = Board()
-board.print_board()
 
 pawn = board.get_piece("a2")
 pawn.move()
-
-board.print_board()
--- pieces.py
+++ pieces.py
@@ -1,6 +1,18 @@
 from abc import ABC, abstractmethod
 from movements import BoardMovements
+import functools
 
+def print_board(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        result = func(self, *args, **kwargs)
+
+        if self.board is not None:
+            self.board.print_board()
+
+        return result
+
+    return wrapper
 
 class BaseChessPiece(ABC):
     def __init__(self, color, name, symbol, identifier):
@@ -93,6 +105,7 @@ class BaseChessPiece(ABC):
     def board(self, value) -> None:
         self.__board = value
 
+    @print_board
     @abstractmethod
     def move(self, movement):
         print(movement)
